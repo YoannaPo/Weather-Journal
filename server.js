3f0130fb7149cde38fa31f59169b8ce61a7d4a6f
YoannaPo <yoannna.pol@gmail.com>
2023-07-10 21:41:08 +0100

    chore: Add bodyParser, express and cors

--- server.js
+++ server.js
@@ -3,9 +3,14 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// Dependencies
+const bodyParser = require('body-parser')
+
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -13,9 +18,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+//to connect the app to the 'cors' package
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+//set variable 
+
